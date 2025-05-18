d65aecccddf05c280e8b1ad2a3222a31ffb1c04e
Michael Farquhar <michaelfarquhar4@gmail.com>
2025-05-18 19:19:48 -0400

    Add container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import { Container } from "@/layout/Container";
 import "./App.css";
 
 function App() {
   return (
-    <main className="container">
-      <h1 className="font-bold">Select file path</h1>
+    <Container>
+      <div className="grid place-items-center h-full">
+        <h1 className="font-bold">Select file path</h1>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(_) => console.log("clicked")}
-          placeholder="Select path..."
-        />
-      </form>
-    </main>
+        <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(_) => console.log("clicked")}
+            placeholder="Select path..."
+          />
+        </form>
+      </div>
+    </Container>
   );
 }
 
